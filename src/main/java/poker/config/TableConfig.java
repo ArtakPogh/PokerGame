bbb79bc1481b8f7b411d77ee35390d1861fc41f6
VIP <poghosyana619@gmail.com>
2026-04-02 17:22:15 +0400

    I added TableConfig

---
+++ src/main/java/poker/config/TableConfig.java
@@ -0,0 +1,9 @@
+public class TableConfig {
+    public static final int MAX_PLAYERS = 6;
+    public static final int MIN_PLAYERS = 2;
+    public static final int STARTING_MONEY = 5000;
+
+    public static final int SMALL_BLIND = 25;
+    public static final int BIG_BLIND = 50; 
+
+}
