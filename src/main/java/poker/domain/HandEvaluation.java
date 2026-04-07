eb88faec84963a9eda15102c5e2c0bde9ab4f0ec
Arthur <arthurtigranyany2@gmail.com>
2026-04-07 18:46:51 +0400

    new enum HandRank some changes to the domain

--- src/main/java/poker/domain/HandEvaluation.java
+++ src/main/java/poker/domain/HandEvaluation.java
@@ -3,11 +3,26 @@ package poker.domain;
 import poker.domain.enums.Suit;
 import poker.domain.enums.Rank;
 import poker.domain.enums.GamePhase;
+import poker.domain.enums.HandRank;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class HandEvaluation{
+    public static HandRank evaluate(Hand hand) {
+        if (isRoyalFlush(hand)) return HandRank.ROYAL_FLUSH;
+        if (isStraightFlush(hand)) return HandRank.STRAIGHT_FLUSH;
+        if (isFourOfAKind(hand)) return HandRank.FOUR_OF_A_KIND;
+        if (isFullHouse(hand)) return HandRank.FULL_HOUSE;
+        if (isFlush(hand)) return HandRank.FLUSH;
+        if (isStraight(hand)) return HandRank.STRAIGHT;
+        if (isThreeOfAKind(hand)) return HandRank.THREE_OF_A_KIND;
+        if (isTwoPair(hand)) return HandRank.TWO_PAIR;
+        if (isPair(hand)) return HandRank.PAIR;
+        return HandRank.HIGH_CARD;
+    }
+    
+    //compare stuff
     public static int evaluate(Hand hand) {
         List<Card> allCards = hand.getAllCards();
         int highestRank = 0;
