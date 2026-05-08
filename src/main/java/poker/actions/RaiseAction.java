26b5c2063f18ee19e0c22ba6f07af402d1071d83
narrmanukyan <narek_manukyan@edu.aua.am>
2026-05-08 17:48:42 +0400

    bug fix in RaiseAction

--- src/main/java/poker/actions/RaiseAction.java
+++ src/main/java/poker/actions/RaiseAction.java
@@ -13,38 +13,25 @@ public class RaiseAction implements Action {
 
     @Override
     public void execute(Player player, GameState gameState) {
+        int totalBet = gameState.getCurrentBet() + raiseAmount;
 
-        int totalBet =
-                gameState.getCurrentBet() + raiseAmount;
-
-        int toPay =
-                totalBet - player.getCurrentBet();
+        int toPay =  totalBet - player.getCurrentBet();
 
         if (raiseAmount <= 0) {
             throw new IllegalArgumentException(
                     "Raise must be positive"
             );
         }
-
         if (toPay > player.getChips()) {
             throw new IllegalStateException(
                     "Not enough chips to raise"
             );
         }
-
         player.bet(toPay);
-
         player.setCurrentBet(totalBet);
-
         gameState.setCurrentBet(totalBet);
-
         gameState.addToPot(toPay);
-
-        System.out.println(
-                player.getName() +
-                " raises to " +
-                totalBet
-        );
+        System.out.println(player.getName() + " raises to " + totalBet);
     }
 
     @Override
